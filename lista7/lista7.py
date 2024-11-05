76620e2872c63ad7e468282bb22ac92a92ffe2e2
Igor Zwirtes <al.igor.zwirtes@impatech.org.br>
2024-11-05 15:17:55 -0300

    Altera a lista 7

--- lista7/lista7.py
+++ lista7/lista7.py
@@ -1,17 +1,155 @@
 # Questão 1
 def find_judge(n, t):
+    people = [[0,0]*n]
     possible_judges = []
-    for i in range(n):
-        judge = True
-        for j in range(n):
-            if i is not j:
-                if t[i][j] == 0:
-                    judge = False
-            else:
-                if t[i][i] == 1:
-                    judge = False
-        if judge:
+    for i in t:
+        people[i[1]-1][0] += 1
+        people[i[0]-1][1] += 1
+    for i in range(len(people)):
+        if people[i][0] == n-1 and people[i][1] == 0:
             possible_judges.append(i)
     if len(possible_judges) != 1:
         return -1
-    return possible_judges[0]
+    return possible_judges[0]+1
+
+# Questão 2
+def minimun_spanning_tree(points):
+    return
+
+# Questão 4
+from typing import Any
+import numpy as np
+import matplotlib.pyplot as plt
+
+epsilon = np.finfo(float).eps
+    
+class Domain:
+    min = None
+    max = None
+
+    def __contains__(self, x):
+        raise NotImplementedError
+    
+    def __repr__(self):
+        raise NotImplementedError
+
+    def __str__(self):
+        return self.__repr__()
+    
+    def copy(self):
+        raise NotImplementedError 
+
+
+class Interval(Domain):
+    def __init__(self, p1, p2):
+        self.inff, self.supp = min(p1, p2), max(p1, p2)
+    
+    @property
+    def min(self):
+        return self.inff
+
+    @property
+    def max(self):
+        return self.supp
+    
+    @property
+    def size(self):
+        return (self.max - self.min)
+    
+    @property
+    def haf(self):
+        return (self.max + self.min)/2.0
+    
+    def __contains__(self, x):
+        return  np.all(np.logical_and(self.inff <= x, x <= self.supp))
+
+    def __str__(self):
+        return f'[{self.inff:2.4f}, {self.supp:2.4f}]' 
+
+    def __repr__(self):
+        return f'[{self.inf!r:2.4f}, {self.supp!r:2.4f}]'
+    
+    def copy(self):
+        return Interval(self.inff, self.supp)
+
+
+class RealFunction:
+    f = None
+    prime = None
+    domain = None
+    
+    def eval_safe(self, x):
+        if self.domain is None or x in self.domain:
+            return self.f(x)
+        else:
+            raise Exception("The number is out of the domain")
+
+    def prime_safe(self, x):
+        if self.domain is None or x in self.domain:
+            return self.prime(x)
+        else:
+            raise Exception("The number is out of the domain")
+        
+    def __call__(self, x) -> float:
+        return self.eval_safe(x)
+    
+    def plot(self):
+        fig, ax = plt.subplots()
+        X = np.linspace(self.domain.min, self.domain.max, 100)
+        Y = self(X)
+        ax.plot(X,Y)
+        return fig, ax
+
+
+def bissect(f: RealFunction, 
+            search_space: Interval, 
+            erroTol: float = 1e-4, 
+            maxItr: int = 1e4, 
+            eps: float = 1e-6 ) -> Interval:
+    count = 0
+    ss = search_space.copy()
+    err = ss.size/2.0
+    fa, fb = f(ss.min), f(ss.max)
+    if fa * fb > -eps:
+        if abs(fa) < eps:
+            return Interval(ss.min, ss.min)
+        elif abs(fb) < eps:
+            return Interval(ss.max, ss.max)
+        else:
+            raise Exception("The interval extremes share the same signal;\n employ the grid search method to locate a valid interval.")
+    while count <= maxItr and err > erroTol:
+        count += 1
+        a, b, m =  ss.min, ss.max, ss.haf
+        fa, fb, fm = f(a), f(b), f(m)
+        if abs(fm) < eps:
+            return Interval(m, m)
+        elif fa * fm < -eps:
+            ss = Interval(a, m)
+        elif fb * fm < -eps:
+            ss = Interval(m, b)
+    return ss
+
+
+def grid_search(f: RealFunction, domain: Interval = None, grid_freq = 8) -> Interval:
+    if domain is not None:
+        D = domain.copy()
+    else:
+        D = f.domain.copy()
+    L1 = np.linspace(D.min, D.max, grid_freq)
+    FL1 = f(L1)
+    TI = FL1[:-1]*FL1[1:]
+    VI = TI <= 0
+    if not np.any(VI):
+        return None
+    idx = np.argmax(VI)
+    return Interval(L1[idx], L1[idx+1])
+
+def newton_root(f: RealFunction, x0: float = 0.0, iterations: int = 0, domain: Interval = None):
+    x = x0
+    for _ in range(iterations):
+        if abs(f(x)) <= 3*epsilon:
+            return x
+        if f.prime(x) < epsilon:
+            raise "Derivative is too small"
+        x = x - (f(x)/f.prime(x))
+    return 'No root found'
